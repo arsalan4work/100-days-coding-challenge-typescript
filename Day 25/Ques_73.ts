c400ac2bb925099b3d0e6ea0a6955550403a2874
arsalan4work <you@example.com>
2024-07-01 16:16:40 +0500

    add files

---
+++ Day 25/Ques_73.ts
@@ -0,0 +1,13 @@
+// Question 73: Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value. 
+// Then, update its value within the same function and log both the initial and updated values.
+
+// This function shows how to assign and update variable values
+function updateVariable() {
+    let number = 15; // Initially assigns the value 15
+    console.log("Initial Value:", number); // Logs the initial value
+
+    number = 30; // Updates the value to 30
+    console.log("Updated Value:", number); // Logs the updated value    
+}
+updateVariable();
+// We assign a value to a variable and then update it, showing how values can change.
