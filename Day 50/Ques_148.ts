280551685fd2c8bf9bf0d64d557354c0eec8c139
arsalan4work <you@example.com>
2024-08-03 20:25:47 +0500

    add files

---
+++ Day 50/Ques_148.ts
@@ -0,0 +1,8 @@
+// Question 148: Demonstrate the use of the setTimeout() function to execute code after a delay.
+
+// Uses setTimeout to log a message after a 3-second delay
+setTimeout(() => {
+   console.log("Hello, World!");
+ }, 3000);
+ console.log("This message is display after 3-seconds delay");
+ // This showcases how to use setTimeout to delay actions in your code.
