d4f23e59a24079ac6f884d75ec04c0cf7e2835c9
arsalan4work <you@example.com>
2024-08-20 14:56:32 +0500

    add files

---
+++ Day 63/any-type.ts
@@ -0,0 +1,14 @@
+/************
+ * ANY TYPE *
+ ************/
+
+let myVariable: any = 42;
+myVariable = "Hello, TypeScript";
+console.log(myVariable)
+
+// However, it's generally advised to avoid using the any type whenever possible in TypeScript for the following reasons:
+
+// Loss of Type Safety:
+// Reduced Code Reliability:
+// Maintenance Challenges:
+// Interferes with Tooling:
