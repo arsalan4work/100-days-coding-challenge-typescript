7a85302564661a6759422223a3a3c186d3f74671
arsalan4work <you@example.com>
2024-08-22 15:02:09 +0500

    add files

---
+++ Day 65/04-Arrow-Function.ts
@@ -0,0 +1,6 @@
+/******************
+ * ARROW FUNCTION *
+ ******************/
+
+const addWithArrow = (num1: number, num2: number): number => num1 + num2;
+console.log(addWithArrow(30,20)); // Output 50
