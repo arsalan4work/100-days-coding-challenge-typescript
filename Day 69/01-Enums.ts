6d7ecdc9765833dcfd32c4f94071a2153ad9070f
arsalan4work <you@example.com>
2024-08-26 15:21:33 +0500

    add files

---
+++ Day 69/01-Enums.ts
@@ -0,0 +1,19 @@
+// Day 69
+
+/*********
+ * ENUMS *
+ *********/
+
+enum Days {
+   Sunday,
+   Monday,
+   Tuesday,
+   Wednesday,
+   Thursday,
+   Friday,
+   Saturday,
+ }
+ 
+ const today: Days = Days.Wednesday;
+ 
+ console.log(`Today is ${Days[today]}`);
