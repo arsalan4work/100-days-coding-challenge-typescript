183dad0da02ae0e96f1bc5b425d603a008686e14
arsalan4work <you@example.com>
2024-08-28 15:03:11 +0500

    add files

---
+++ Day 73/01-Type-Assertions.ts
@@ -0,0 +1,10 @@
+// Day 73
+
+/*******************
+ * TYPE ASSERTIONS *
+ *******************/
+
+let data: any = "1000";
+
+// Type assertion to treat "data" as a string
+console.log((data as string).repeat(3));
