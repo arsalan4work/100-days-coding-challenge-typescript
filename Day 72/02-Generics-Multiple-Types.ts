34042f01483b96a5adcf29457bb217ffaa301fd8
arsalan4work <you@example.com>
2024-08-28 15:00:09 +0500

    add files

---
+++ Day 72/02-Generics-Multiple-Types.ts
@@ -0,0 +1,41 @@
+// Day 72
+
+/***************************
+ * GENERICS MULTIPLE TYPES *
+ ***************************/
+
+// Generic Function `returnTypeEx`
+function returnTypeEx<T>(val: T): T {
+   return val;
+ }
+ 
+ console.log(returnTypeEx<number>(100)); // Returns: 100 (number)
+ console.log(returnTypeEx<string>("Arsalan")); // Returns: "Arsalan" (string)
+ 
+ // Arrow Function with Generics `returnTypeArrowSyntax`
+ const returnTypeArrowSyntax = <T>(val: T): T => val;
+ 
+ console.log(returnTypeArrowSyntax<number>(100)); // Returns: 100 (number)
+ console.log(returnTypeArrowSyntax<string>("Elzero")); // Returns: "Elzero" (string)
+ 
+ // Generic Function `testType`
+ function testType<T>(val: T): string {
+   return `The Name Is ${val} And Type Is ${typeof val}`;
+ }
+ 
+ console.log(testType<number>(100));
+ // Returns: "The Value Is 100 And Type Is number"
+ 
+ console.log(testType<string>("Arsalan"));
+ // Returns: "The Value Is Arsalan And Type Is string"
+ 
+ // Generic Function `multipleTypes`
+ function multipleTypes<T, S>(valueOne: T, valueTwo: S): string {
+   return `The First Value Is ${valueOne} And Second Value ${valueTwo}`;
+ }
+ 
+ console.log(multipleTypes<string, number>("Osama", 100));
+ // Returns: "The First Value Is Osama And Second Value 100"
+ 
+ console.log(multipleTypes<string, boolean>("Elzero", true));
+ // Returns: "The First Value Is Elzero And Second Value true"
